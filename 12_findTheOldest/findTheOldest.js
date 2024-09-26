6c37fe684dfae0fe62aa293e01ab9320c210eeae
nawaf al-mehdhar <108904563+nawaf-dh@users.noreply.github.com>
2024-09-26 15:10:12 +0300

    finished all exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
+  return people.reduce((oldest, currentPerson) => {
+    const currentPersonAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
 
+    return oldestAge < currentPersonAge ? currentPerson : oldest;
+  });
 };
 
 // Do not edit below this line
