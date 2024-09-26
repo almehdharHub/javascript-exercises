475754449fa632b0f3fcadcc2dc7963fe05b4a09
nawaf al-mehdhar <108904563+nawaf-dh@users.noreply.github.com>
2024-09-26 10:11:25 +0300

    exercises 5 to 8

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
+const leapYears = function (yr) {
+  if (yr % 100 == 0 && yr % 400 == 0) return true;
+  if (yr % 100 != 0 && yr % 4 == 0) return true;
 
+  return false;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahr) {
+  result = (fahr - 32) * (5 / 9);
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cel) {
+  result = cel * (9 / 5) + 32;
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
