d6d379ee5d7b7f97780860044b454ad8f0104d3e
nawaf al-mehdhar <108904563+nawaf-dh@users.noreply.github.com>
2024-09-26 14:53:59 +0300

    exercise 9, 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+// const palindromes = function (str) {
+//   let reversed = str
+//     .toLowerCase()
+//     .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+//     .split("")
+//     .reverse()
+//     .join("");
+//   let original = str
+//     .toLowerCase()
+//     .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+//     .split("")
+//     .join("");
 
-};
-
+//   return reversed === original ? true : false;
+// };
+function palindromes(str) {
+  const cleanStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  console.log(cleanStr);
+  return cleanStr === cleanStr.split("").reverse().join("");
+}
+console.log(palindromes("hello world"));
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  // at fun1 return 1
+  // at fun2 return 1
+  // at fun3 return fun1 + fun2 = 1 + 1 = 2
+  // at fun4 return fun3 + fun2 = 2 + 1 = 3
+  parseInt(n);
+  if (n < 0) {
+    return "OOPS";
+  } else if (n == 0) {
+    return 0;
+  } else if (n <= 2 && n > 0) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
 };
 
 // Do not edit below this line
